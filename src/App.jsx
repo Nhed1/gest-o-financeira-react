f01aeac0e6574c2ff43c97348a4f37a8b3ac72fe
Leonardo Garreto <leonardogarreto@gmail.com>
2022-01-09 11:21:39 -0300

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import {Header} from './components/Header.jsx'
-import {History} from './components/History.jsx'
-import {Form} from './components/Form.jsx'
+import './app.css'
 import { useState } from 'react'
-import './app.scss'
 
-function App() {
-  const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0) 
-  function handleValueSubmit(value){
-    setValue(value)
-  }
+function App() {  
   return (
-    <div className="App">
-      <Header></Header>
-      <History></History>
-      <Form></Form>
+    <div>
     </div>
   );
 }
